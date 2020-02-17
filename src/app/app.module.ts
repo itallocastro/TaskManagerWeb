759f26b7ad6152fefc36132a83db7e06a3aff109
Itallo <itallopcastro@gmail.com>
2020-02-17 20:43:51 -0300

    editor do task implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgxEditorModule } from 'ngx-editor';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {HttpClientModule} from '@angular/common/http';
+import { EditorTextComponent } from './editor-text/editor-text.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorTextComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxEditorModule,
+    FormsModule,
+    AngularFontAwesomeModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/editor-text/editor-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-text',
+  templateUrl: './editor-text.component.html',
+  styleUrls: ['./editor-text.component.scss']
+})
+export class EditorTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
